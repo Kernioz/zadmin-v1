3a6c2a6b9ebb218ef039af87bd6354f7268d978f
kernioz <mail@kernioz.com>
2022-11-24 17:27:08 +0100

    kernioz

--- module/staffmenu/cl_staff.lua
+++ module/staffmenu/cl_staff.lua
@@ -228,7 +228,7 @@ function Admin:OpenMenu()
                    
                     RageUI.ButtonWithStyle(" Freeze le joueur", nil, {RightLabel = ""}, true, function(_, _, s) 
                         if s then 
-                            ExecuteCommand("freeze " .. Admin.targetId.serverId)
+                            ExecuteCommand("freezed " .. Admin.targetId.serverId)
                         end 
                     end)
                     RageUI.ButtonWithStyle(" Screenshot", nil, {RightLabel = ""}, true, function(_, _, s) 
