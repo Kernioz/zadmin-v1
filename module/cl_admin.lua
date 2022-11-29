53eb9b3e93c68c0e3a151a780f61b5ce53570bad
kernioz <mail@kernioz.com>
2022-11-29 20:31:57 +0100

    kernioz 2

--- module/cl_admin.lua
+++ module/cl_admin.lua
@@ -439,7 +439,7 @@ whileShowName = function()
                     
                     SetMpGamerTagVisibility(gamerTags[v], Admin.AllTags.AUDIO_ICON, NetworkIsPlayerTalking(v))
                     SetMpGamerTagAlpha(gamerTags[v], Admin.AllTags.AUDIO_ICON, 255)
-                    SetMpGamerTagName(gamerTags[v], "[" .. value.userId .. "] - " .. GetPlayerName(v))
+                    SetMpGamerTagName(gamerTags[v], "[" .. myName.userId .. "] - " .. GetPlayerName(v))
     
                     SetMpGamerTagVisibility(gamerTags[v], Admin.AllTags.INV_IF_PED_FOLLOWING, not IsPedInAnyVehicle(otherPed, false))
                     SetMpGamerTagAlpha(gamerTags[v], Admin.AllTags.INV_IF_PED_FOLLOWING, 255)
